3e5902f08b32bfa99692818f5d65d068b322b71f
SamruddhiGupta <8g16samruddhigupta@gmail.com>
2026-01-04 20:49:05 +0530

    Add files via upload

---
+++ Final.js
@@ -0,0 +1,26 @@
+document.getElementById("button.macro").onclick = function() {
+  window.location.href= "Macro.html";
+}
+
+document.getElementById("button.architectural").onclick = function() {
+  window.location.href= "Architectural.html";
+}
+
+document.getElementById("button.wildlife").onclick = function() {
+  window.location.href= "Wildlife.html";
+}
+
+document.getElementById("button.nature").onclick = function() {
+  window.location.href= "Nature.html";
+}
+
+document.getElementById("button.monochrome").onclick = function() {
+  window.location.href= "Monochrome.html";
+}
+
+document.getElementById("button.food").onclick = function() {
+  window.location.href= "Food.html";
+}
+
+
+
